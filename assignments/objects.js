8b870c577ee20c3194660f9b5273fd1ca3e9254e
Jkeslar21 <46494969+Jkeslar21@users.noreply.github.com>
2019-01-18 21:22:34 -0700

    stumped

--- assignments/objects.js
+++ assignments/objects.js
@@ -29,7 +29,10 @@ const second = {
   'email': 'kdiben1@tinypic.com',
   'name': 'Kennan',
   'gender': 'M',
-}
+  'speak': function(){
+    console.log('Hello! My name is Kennan!');
+  }
+};
 const third = {
   'id': 3,
   'email': 'kmummery2@wikimedia.org',
@@ -65,7 +68,7 @@ console.log(fifth.gender)
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
-
+second.speak()
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
 
