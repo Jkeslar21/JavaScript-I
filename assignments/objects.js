dcc42d6a695d1e488f9603e9b23c79c692d6ddb7
Jkeslar21 <46494969+Jkeslar21@users.noreply.github.com>
2019-01-21 13:25:55 -0700

    challenge 3

--- assignments/objects.js
+++ assignments/objects.js
@@ -30,7 +30,7 @@ const second = {
   'name': 'Kennan',
   'gender': 'M',
   'speak': function(){
-    console.log('Hello! My name is Kennan!');
+    return 'Hello! My name is Kennan!';
   }
 };
 const third = {
@@ -50,6 +50,9 @@ const fifth = {
   'email': 'adaine5@samsung.com',
   'name': 'Antonietta',
   'gender': 'F',
+  'product': function(x, y){
+    return x * y;
+  }
 }
 
 // ==== Challenge 2: Reading Object Data ==== 
@@ -68,10 +71,10 @@ console.log(fifth.gender)
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
-second.speak()
+console.log(second.speak());
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
-
+console.log(fifth.product(2,4));
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
 // ==== Stretch Challenge: Nested Objects and the this keyword ==== 
